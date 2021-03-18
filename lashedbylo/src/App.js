4d13d263893fbf35be754e54bf99edd8e3603bd5
XavierTwitty <x.twitty@gmail.com>
2021-03-18 17:03:56 -0400

    all pages rendering by themselves

--- lashedbylo/src/App.js
+++ lashedbylo/src/App.js
@@ -2,7 +2,8 @@ import logo from "./logo.svg";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Services from "./components/Services";
-
+import Training from "./components/Training";
+import Contact from "./components/Contact";
 import "./App.css";
 
 function App() {
@@ -16,21 +17,21 @@ function App() {
               <Link to="/"> Home </Link>
             </li>
             <li>
-              <Link to="services"> Services </Link>
+              <Link to="Services"> Services </Link>
             </li>
             <li>
-              <Link to="training"> Training </Link>
+              <Link to="Training"> Training </Link>
             </li>
             <li>
-              <Link to="contact"> Contact </Link>
+              <Link to="Contact"> Contact </Link>
             </li>
           </ul>
         </header>
         <Switch>
-          <Route>
-            <Home exact path="/" />
-          </Route>
-          <Route path="services" component={Services} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Services" component={Services} />
+          <Route exact path="/Training" component={Training} />
+          <Route exact path="/Contact" component={Contact} />
         </Switch>
       </div>
     </Router>
--- lashedbylo/src/components/Contact.js
+++ lashedbylo/src/components/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Services() {
   return (
     <div>
-      <h1> can you see me </h1>
+      <h1> can you see me in contact </h1>
     </div>
   );
 }
