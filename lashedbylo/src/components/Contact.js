06ccbb2d3322330f5be9b0329ed8524a280bfa5d
XavierTwitty <x.twitty@gmail.com>
2021-03-22 19:30:36 -0400

    form layout complete

--- lashedbylo/src/components/Contact.js
+++ lashedbylo/src/components/Contact.js
@@ -3,53 +3,59 @@ import React from "react";
 export default function Contact() {
   return (
     <div>
-      <form>
-        <div>
-          <label htmlFor="name">
-            <input
-              type="text"
-              placeholder="Name"
-              name="name"
-              value={""}
-              onChange={""}
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="email">
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              value={""}
-              onChange={""}
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="subject">
-            <input
-              type="text"
-              placeholder="Subject"
-              name="name"
-              value={""}
-              onChange={""}
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="textArea">
-            <textarea
-              name="textarea"
-              placeholder="Type your message here..."
-              value={""}
-              onChange={""}
-            />
-          </label>
+      <div className="container">
+        <div className="contact_img">
+          <h1>image placeholder</h1>
         </div>
 
-        <button> Submit </button>
-      </form>
+        <form>
+          <div>
+            <label htmlFor="name">
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={""}
+                onChange={""}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="email">
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={""}
+                onChange={""}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="subject">
+              <input
+                type="text"
+                placeholder="Subject"
+                name="name"
+                value={""}
+                onChange={""}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="textArea">
+              <textarea
+                name="textarea"
+                placeholder="Type your message here..."
+                value={""}
+                onChange={""}
+              />
+            </label>
+          </div>
+
+          <button> Submit </button>
+        </form>
+      </div>
     </div>
   );
 }
